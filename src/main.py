0e02380ef72d347d8f9d759366c36d07961c2c7b
DebarghaG <maildebargha@gmail.com>
2020-11-19 14:14:41 +0530

    Adding vanilla querying

--- src/main.py
+++ src/main.py
@@ -2,4 +2,5 @@ from parse import Parser
 
 if __name__ == '__main__':
     parsewiki = Parser("../data/WestburyLab.Wikipedia.Corpus.txt")
-    parsewiki.parse()
+    #parsewiki.parse()
+    parsewiki.vanillaQuery("Debargha")
--- src/parse.py
+++ src/parse.py
@@ -29,3 +29,28 @@ class Parser:
         stop = time.time()
         print("Read in "+str(articleid) +" documents")
         print("Took >> "+ str(stop-start) + " seconds")
+
+    def vanillaQuery(self, query):
+        #print("Reading file from >> "+ self.dump_location)
+        start = time.time()
+        file = open(self.dump_location, 'r')
+        article      = ""
+        articleid    = 0
+        while True:
+            line = file.readline()
+            if not line:
+                break
+            if line=="---END.OF.DOCUMENT---\n":
+                articleid += 1
+                article = ""
+                continue
+            article = article + line
+
+            if query in article:
+                print(article)
+                break
+
+        stop = time.time()
+
+        print("Checked in "+str(articleid) +" documents")
+        print("Took >> "+ str(stop-start) + " seconds")
